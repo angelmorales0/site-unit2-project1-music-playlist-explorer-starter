0a5a0c37e87fafda3cd514df6b56d1eddf4d0820
Angel Daniel Morales <angelmorales@meta.com>
2025-06-06 12:26:47 -0700

    finished required tasks

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,21 +1,37 @@
 const modal = document.getElementById("first-modal")
 const span = document.querySelector('.close')
+let likes = 0
 
+
+let songsArray= []
+let playlistArray = []
 
 function openModal(playlist) {
+    
    document.getElementById('playlist-title').innerText = playlist.title;
    document.getElementById('playlist-owner').innerText= playlist.owner;
-    // NEED TO PUPULATE MODAL HTML FIRST
-    
+   document.getElementById('modal_img').src= playlist.playlist_art
+
+    let songsArray= playlist.songs
+
    modal.style.display = "block";
-   console.log(playlist)
-   loadSongData(playlist.songs)
+   loadSongData(songsArray)
+
+   const shuffleBtn = document.getElementById('shuffle-btn');
+ 
+    shuffleBtn.onclick = () => {
+    shuffleArray(songsArray);
+    loadSongData(songsArray);
+    };
+    
 
 }
 //Close modal 
 
+if (span){
  span.onclick = function(){
     modal.style.display = "none"
+}
 }
 
 window.onClick = function(event){
@@ -25,7 +41,9 @@ window.onClick = function(event){
 }
 
 
-function loadPlaylistData(){
+
+
+async function  loadPlaylistData(){
      const playlists = document.querySelector(".playlist-cards");
     
     fetch('data/data.json')
@@ -38,33 +56,59 @@ function loadPlaylistData(){
             return accumulator + makePlaylist(lmnt)    
         }, "");
         playlists.innerHTML =returnString
+        const hearts = document.querySelectorAll('.fa-regular.fa-heart')
+        hearts.forEach(heart =>{
+            heart.addEventListener("click", toggleLike)
+        })
+
+
+function toggleLike(event){
+    const heart = event.target
+    isLiked = ( 1 == likes)
+
+    if (isLiked){
+        likes --
+        heart.style.color= "black"
+
+    } else{
+        likes ++
+            heart.style.color = 'red';
+
+    }
+    heart.innerText = " "+ likes + " likes"
+}
     } )
     
 }
 
 function makePlaylist(props){
-    console.log(`${JSON.stringify(props.songs)}`)
-    //CURRENT GOAL -> GET THESE PLAYLIST CARDS TO BE CREATED ON DEMAND BY JSON 
+
+    
     return `<article
-          onClick='openModal({owner:"${props.playlist_author}", title:"${props.playlist_name}",songs:${JSON.stringify(props.songs)}})'
+     
           class="card">
-          <img class="card-img" src="assets/img/playlist.png" />
-          <div class="card-content" width="150px">
+<img onClick='openModal({owner:"${props.playlist_author}", title:"${props.playlist_name}", songs:${JSON.stringify(props.songs)}, playlist_art:"${props.playlist_art}"})' class="card-img" src="${props.playlist_art}" />          <div class="card-content" width="150px">
             <h3>${props.playlist_name}</h3>
             <p>${props.playlist_author}</p>
             <div id="likes">
-            <i id="heart" class="fa-regular fa-heart"> Likes</i>
+            <i id="heart" class="fa-regular fa-heart"> ${likes} Likes</i>
             </div>
           </div>
         </article>`
 }
-window.addEventListener('DOMContentLoaded', () =>{
-    loadPlaylistData()
+window.addEventListener('DOMContentLoaded', async () =>{
+    if (span){
+    await loadPlaylistData()
+    }
+    const featured = await getRandomPlaylist();
+    displayFeaturedPlaylist(featured);
+ 
+  
+
 });
 
-function loadSongData(songList){ // WHY IS IT A STRING 
+function loadSongData(songList){
     const songs = document.querySelector(".song-card")
-    console.log(songList)
 
     let songData = songList.reduce((accumulator, song) =>{
         return accumulator + makeSongCard(song)
@@ -77,14 +121,13 @@ function loadSongData(songList){ // WHY IS IT A STRING
 
 
 function makeSongCard(props){
-    console.log(props)
-    return `<img id="song-img" src="assets/img/song.png" />
-
+    return `
             <div id="song-card-container">
+            <img id="song-img" src="${props.img}" />
               <div class="song-card-body">
-                <h4 id="song-title">${props.title}</h4>
-                <h4 id="artist">${props.artist}</h4>
-                <h4 id="album">${props.album}</h4>
+                <h4 id="song-title">Title: ${props.title}</h4>
+                <h4 id="artist">Artist: ${props.artist}</h4>
+                <h4 id="album">Album: ${props.album}</h4>
               </div>
 
               <div id="time">
@@ -93,7 +136,41 @@ function makeSongCard(props){
             </div>`
 }
 
-/**
- * TODOS 
- * Make row gap black to seperate songs within the playlists
- */
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+
+function getRandomPlaylist() {
+     
+    return fetch('data/data.json')
+    .then((response) => {
+        
+    return response.json()} ) 
+    .then ( playlists =>{
+          const randomIndex = Math.floor(Math.random() * playlists.length);
+            const randomPlaylist = playlists[randomIndex]
+            return randomPlaylist
+        })
+
+  
+}
+
+function displayFeaturedPlaylist(playlist) {    
+  document.getElementById('featured-img').src = `${playlist.playlist_art}`;
+  document.getElementById('featured-name').textContent = playlist.playlist_name;
+  document.getElementById('featured-author').textContent = `By ${playlist.playlist_author}`;
+  const songList = document.getElementById('featured-songs');
+  songList.innerHTML = '';
+   playlist.songs.forEach(song => {
+    const songCard = makeSongCard(song);
+    songList.innerHTML += songCard; 
+  });
+}
+
